137e8c1ff70885e022b6acc928ad5683060e5c8c
Marcus Gibeson <133388744+MarcusGibeson@users.noreply.github.com>
2024-02-11 01:28:42 -0500

    commit

--- src/main/java/budgetproject/budgetproject/Repositories/BillRepository.java
+++ src/main/java/budgetproject/budgetproject/Repositories/BillRepository.java
@@ -1,5 +1,10 @@
 package budgetproject.budgetproject.Repositories;
 
-public interface BillRepository {
-    
+import org.springframework.data.repository.CrudRepository;
+
+import budgetproject.budgetproject.Models.Bill;
+
+public interface BillRepository extends CrudRepository<Bill, Long> {
+    Bill findById(long id);
+    Bill findByName(String name);
 }
--- src/main/java/budgetproject/budgetproject/Services/BillService.java
+++ src/main/java/budgetproject/budgetproject/Services/BillService.java
@@ -1,5 +1,56 @@
 package budgetproject.budgetproject.Services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import budgetproject.budgetproject.Models.Bill;
+import budgetproject.budgetproject.Repositories.BillRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
 public class BillService {
     
+    @Autowired
+    private final BillRepository billRepository;
+
+    public BillService(BillRepository billRepository) {
+        this.billRepository = billRepository;
+    }
+
+    public Bill save (Bill bill) {
+        return billRepository.save(bill);
+    }
+
+    public Iterable<Bill> retrieveAllBills() {
+        return billRepository.findAll();
+    }
+
+    public Bill retrieveBillById(Long id) throws Exception{
+        try {
+            return billRepository.findById(id).get();
+        } catch (Exception e) {
+            throw new Exception("Bill not found");
+        }
+    }
+
+    public Bill getBillByName(String name) throws Exception {
+        try{
+            Bill bill = billRepository.findByName(name);
+            return bill;
+        } catch(Exception e){
+            throw new Exception("Bill not found");
+        }
+    }
+
+    public void deleteBill(Long billId) {
+        Optional<Bill> billOptional = billRepository.findById(billId);
+        if (billOptional.isPresent()) {
+            Bill billToDelete = billOptional.get();
+            billRepository.delete(billToDelete);
+        } else {
+            throw new EntityNotFoundException("Bill with Id " + billId + " was not found.");
+        }
+    }
 }
