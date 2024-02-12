0ed0e8e26acb26aebd8f5b170a499058f52fa4ae
Marcus Gibeson <133388744+MarcusGibeson@users.noreply.github.com>
2024-02-11 21:08:23 -0500

    commit

--- src/main/java/budgetproject/budgetproject/Models/Income.java
+++ src/main/java/budgetproject/budgetproject/Models/Income.java
@@ -1,8 +1,29 @@
 package budgetproject.budgetproject.Models;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Income {
+    
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private int amount;
+    private LocalDate datePaid;
+    
+    public Income() {
+    }
+
+    public Income(String name, int amount, LocalDate datePaid) {
+        this.name = name;
+        this.amount = amount;
+        this.datePaid = datePaid;
+    }
+
     
 }
--- src/main/java/budgetproject/budgetproject/Models/User.java
+++ src/main/java/budgetproject/budgetproject/Models/User.java
@@ -1,8 +1,15 @@
 package budgetproject.budgetproject.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
+    
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
     
 }
